615d0c9c3f7095fc4d98d67b5e73769ac46ebafa
faisl8500-cmyk <faisl8500@gmail.com>
2026-05-08 03:51:46 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'far-almorqb-v3';
+const urlsToCache = ['/','/index.html'];
+
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(name) {
+                    if (name !== CACHE_NAME) return caches.delete(name);
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).then(function(response) {
+            var clone = response.clone();
+            caches.open(CACHE_NAME).then(function(cache) {
+                cache.put(event.request, clone);
+            });
+            return response;
+        }).catch(function() {
+            return caches.match(event.request);
+        })
+    );
+});
