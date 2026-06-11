7741fafe35d847f1115f98bc1c5d93ff5ccb4000
faisl8500-cmyk <faisl8500@gmail.com>
2026-06-12 01:16:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,48 @@
-const CACHE_NAME = 'far-almorqb-v3';
-const urlsToCache = ['/','/index.html'];
+const CACHE_NAME = 'far-almorqb-v2.1.1';
+const urlsToCache = [
+  '/faisl8500-cmyk5/',
+  '/faisl8500-cmyk5/index.html',
+  '/faisl8500-cmyk5/manifest.json',
+  '/faisl8500-cmyk5/logo.png',
+  '/faisl8500-cmyk5/icon-192.png',
+  '/faisl8500-cmyk5/icon-512.png'
+];
 
-self.addEventListener('install', function(event) {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+// تثبيت Service Worker وتخزين الملفات الأساسية
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(name) {
-                    if (name !== CACHE_NAME) return caches.delete(name);
-                })
-            );
-        })
-    );
-    return self.clients.claim();
+// استراتيجية: محاولة الشبكة أولاً، ثم العودة إلى Cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        fetch(event.request).then(function(response) {
-            var clone = response.clone();
-            caches.open(CACHE_NAME).then(function(cache) {
-                cache.put(event.request, clone);
-            });
-            return response;
-        }).catch(function() {
-            return caches.match(event.request);
+// تنظيف التخزين المؤقت القديم عند تنشيط Service Worker جديد
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
         })
-    );
+      );
+    }).then(() => self.clients.claim())
+  );
 });
